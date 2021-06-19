4c6b6ce541e2c949f0dd966b487fa67f629fab3e
pranaytejareddy1996 <ch.pranaytejareddy@gmail.com>
2021-06-19 20:01:08 +0530

    first commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,8 +22,8 @@ const Footer = ({ data }) => {
           <ul className="copyright">
             <li>
               Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+              <a title="Pranay Teja Reddy" >
+              Pranay Teja Reddy
               </a>
             </li>
           </ul>
